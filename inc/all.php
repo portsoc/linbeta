0a1178f64af8d32634c9dbd781ee1b3c21a8b40a
tpunt <tpunt@hotmail.co.uk>
2014-10-23 21:18:02 +0100

    Fixed link for file_exists() function checking

--- inc/all.php
+++ inc/all.php
@@ -3,7 +3,7 @@
     include __DIR__.'/io.php';
 
     // io include needs to have happened for debug fn to exist.
-    if(!file_exists('config.php')) {
+    if(!file_exists(__DIR__.'/config.php')) {
         debug("<b>Hold on Sparky!</b> You need to copy the
             contents of the <code>config_sample.php</code>
             file into a new <code>config.php</code> file and fill in the database
