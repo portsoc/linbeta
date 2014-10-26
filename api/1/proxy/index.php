9862b1bd879669bd775c48fb48d3130b84803a25
ear1grey <rjb@port.ac.uk>
2014-10-26 17:41:20 +0000

    adds /api/1/proxy/ URI that retrieves a uri and returns its title and keywords so that they can be edited by the user and then added to the DB - let's not make the user do any typing they don't *have* to.

---
+++ api/1/proxy/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$url = urldecode ( trim($_REQUEST['url']) );
+$tags = get_meta_tags($url);
+
+// title isn't covered by get_meta_tags to pull it from the page content
+$str = file_get_contents($url);
+preg_match("/<title ?.*>(.*)<\/title>/", $str, $matches);
+$tags["title"] = $matches[1];
+
+echo json_encode($tags);
+?>
